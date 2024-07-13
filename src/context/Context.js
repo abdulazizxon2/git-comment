fcd8c07ffc3ad212da1708cb5e863962d2439a0a
abdulazizxon2 <boydadayevabbos@gmail.com>
2024-07-13 17:30:23 +0500

    ohirgi

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,6 +1,6 @@
 import React, { createContext } from "react";
 export let Context = createContext();
 export default function ContextP({ children }) {
-  let text = "salom olam";
+  let text = "salom olamim";
   return <Context.Provider value={{ text }}>{children}</Context.Provider>;
 }
